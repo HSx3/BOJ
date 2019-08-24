6067d7bd9ae0d19e014fb3a7d792b829c6eea6cf
S <s@S.local>
2019-08-24 19:51:14 +0900

    2206_BFS

--- 벽부수고이동하기_BFS.py
+++ 벽부수고이동하기_BFS.py
@@ -22,24 +22,27 @@ def BFS(x, y, distance, chance, temp):
             if nx < 0 or nx >= N or ny < 0 or ny >= M:
                 continue
             # 길
-            if data[nx][ny] != 0 and chance == 0:
+            if temp[nx][ny] != 0 and chance == 0:
                 continue
             # chance
-            if data[nx][ny] != 0 and chance == 1:
-                chance = 0
+            if temp[nx][ny] != 0 and chance == 1:
+                # chance = 0
                 temp[nx][ny] = 0
                 visited[nx][ny] = distance+1
-                q.append((nx, ny, distance+1, chance, temp))
+                q.append((nx, ny, distance+1, chance-1, temp))
                 chance = 1
             # 탈출
             if nx == N-1 and ny == M-1:
+                visited[nx][ny] = distance+1
+                for i in visited:
+                    print(i)
                 if result > distance+1:
                     result = distance+1
             # 이동
-            if data[nx][ny] == 0 and visited[nx][ny] == 0:
+            if temp[nx][ny] == 0 and visited[nx][ny] == 0:
                 visited[nx][ny] = distance+1
-                q.append((nx, ny, distance+1, chance))
-
+                q.append((nx, ny, distance+1, chance, temp))
+    return -1
 
 
 
