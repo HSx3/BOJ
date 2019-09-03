3340c9e71ffcfcd2c8698131c541447c07f4fc88
HSx3 <hs.ssafy@gmail.com>
2019-09-03 13:28:23 +0900

    15651

--- N과M(3).py
+++ N과M(3).py
@@ -1,5 +1,25 @@
 import sys
 sys.stdin = open('N과M(3)_input.txt')
 
+
+def DFS(no):
+    if no >= M:
+        for i in range(N):
+            if b[i] != 0:
+                print(b[i], end=' ')
+        print()
+        return
+    for i in range(N):
+        check[i] = 1
+        b[no] = data[i]
+        DFS(no + 1)
+        check[i] = 0
+
+
 N, M = map(int, input().split())
-print(N, M)
+
+data = list(range(1, N+1))
+b = [0] * N
+check = [0] * N
+
+DFS(0)
