15f01355114755f0e0ab1422d7d5f97c34824482
HSx3 <hs.ssafy@gmail.com>
2019-09-03 12:14:33 +0900

    15651

---
+++ N과M(3).py
@@ -0,0 +1,5 @@
+import sys
+sys.stdin = open('N과M(3)_input.txt')
+
+N, M = map(int, input().split())
+print(N, M)
--- test.py
+++ test.py
@@ -1,2 +1,5 @@
 import sys
 sys.stdin = open("test_input.txt")
+
+data = input()
+print(data)
