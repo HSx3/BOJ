c3f3b0293476f0f0c658766dad4b3abd9ada3016
HSx3 <hs.ssafy@gmail.com>
2019-09-04 10:37:52 +0900

    14501

--- 퇴사.py
+++ 퇴사.py
@@ -1,52 +1,24 @@
 import sys
 sys.stdin = open('퇴사_input.txt')
 
+
+def backtrack(day, income):
+    global ans
+    ans = max(ans, income)
+    if day > N:
+        return
+    if day + data[day][1] - 1 <= N:
+        backtrack(day+data[day][1], income+data[day][2])
+    backtrack(day+1, income)
+
+
 N = int(input())
 schedule = [0] * (N+1)
-print(schedule)
-# data = [(0, 0, 0)]
-data = []
+data = [(0, 0, 0)]
 for i in range(1, N+1):
     Ti, Pi = map(int, input().split())
     data.append((i, Ti, Pi))
-print(data)
-# sort_data = sorted(data, key=lambda x:x[2], reverse=True)
-# print(sort_data)
-max_income = 0
-income = 0
-# i = N
-# while i > 0:
 
-for i in range(N, 0, -1):
-    if data[i-1][0] + data[i-1][1]-1 <= N:
-        print(i, schedule[data[i-1][0]:data[i-1][0]+data[i-1][1]])
-        if 99 in schedule[data[i-1][0]:data[i-1][0]+data[i-1][1]]:
-            continue
-        else:
-            for j in range(0, data[i-1][1]):
-                schedule[data[i-1][0]+j] = 99
-            income += data[i-1][2]
-            # print(i)
-            # print(schedule)
-if max_income < income:
-    max_income = income
-print(max_income)
-income = 0
-schedule = [0] * (N+1)
-for i in range(1, N):
-    if data[i-1][0] + data[i-1][1]-1 <= N:
-        print(i, schedule[data[i-1][0]:data[i-1][0]+data[i-1][1]])
-        # print(data[i-1][0], data[i-1][0]+data[i-1][1])
-        if 99 in schedule[data[i-1][0]:data[i-1][0]+data[i-1][1]]:
-            continue
-        else:
-            for j in range(0, data[i-1][1]):
-                schedule[data[i-1][0]+j] = 99
-            income += data[i-1][2]
-            print(schedule, income)
-            # print(i)
-            # print(schedule)
-
-if max_income < income:
-    max_income = income
-print(max_income)
+ans = 0
+backtrack(1, 0)
+print(ans)
